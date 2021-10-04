24db3d7ce5c5712ae0f6772dae92b7d49c458896
HomemadeToast <54961512+HomemadeToast57@users.noreply.github.com>
2021-10-04 10:21:21 -0400

    Added game logic for full game loop 🔁

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -15,6 +16,14 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(win.isActiveAndEnabled)
+        {
+            Time.timeScale = 0;
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                Time.timeScale = 1;
+            }
+        }
     }
 }
